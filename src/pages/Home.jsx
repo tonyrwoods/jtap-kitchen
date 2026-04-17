9721f80200c40168a7c149b25ec9470752fa49d4
Anthony Woods <tonyrwoods@gmail.com>
2026-04-17 03:14:38 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,23 @@
 import useSeoMeta from "../hooks/useSeoMeta";
 import HeroSection from "../components/HeroSection";
-import SmartKitchenSection from "../components/SmartKitchenSection";
 import GallerySection from "../components/GallerySection";
 import InstagramSection from "../components/InstagramSection";
 import TeamSection from "../components/TeamSection";
-import ChefsSpotlight from "../components/ChefsSpotlight";
 import MenuSection from "../components/MenuSection";
 import AboutSection from "../components/AboutSection";
 import TestimonialsSection from "../components/TestimonialsSection";
-import FeedbackSection from "../components/FeedbackSection";
-import ReservationSection from "../components/ReservationSection";
 
 export default function Home() {
   useSeoMeta("home");
   return (
     <>
       <HeroSection />
-      <SmartKitchenSection />
       <MenuSection />
       <AboutSection />
       <TeamSection />
-      <ChefsSpotlight />
       <GallerySection />
       <InstagramSection />
       <TestimonialsSection />
-      <FeedbackSection />
-      <ReservationSection />
     </>
   );
 }
